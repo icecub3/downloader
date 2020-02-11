5fff5bc457582f53026205ad688fdb06ba279555
Fatih <fkoc.041@gmail.com>
2020-02-11 09:09:20 +0300

    Simple Command Line interface added.

--- downloader.py
+++ downloader.py
@@ -4,30 +4,27 @@ import os
 import requests
 import time
 
-# Specify the URL of the page that you want to search
-archive_url = "http://www.dinimizislam.com/detay.asp?Aid=13613"
 
-
-# Crawls into given archive_url and collects file URLs with '.mp3'.
-def get_mp3_links():
+# Crawls into given archive_url and collects file URLs with given file type
+# TODO: It needs to be reworked
+def get_file_links(archive_url, file_type):
     response = requests.get(archive_url)
     soup = BeautifulSoup(response.content, 'html5lib')  # Using BeautifulSoup library to pull the data out
     links = soup.findAll('a')                           # Pull all <a> tags from the web page
-    mp3_links = []                                      # Filter the data and collect the .mp3 links
+    links_to_download = []                              # Filter the data and collect the given extension links
     for link in links:
         if 'href' in link.attrs.keys():
-            if '.mp3' in link.attrs['href']:
+            if file_type in link.attrs['href']:
                 # print("DEBUG::link.attrs['href']:" + str(link.attrs['href']))
-                mp3_links += [link.attrs['href']]
-    print(str(len(mp3_links)) + " links found.")
-    return mp3_links
+                links_to_download += [link.attrs['href']]
+    print(str(len(links_to_download)) + " links found.")
+    return links_to_download
 
 
 # Downloads given files to the '/downloaded' directory.
 def download_mp3_series(mp3_links):
     print("Download started.")
     all_files_timer_start = time.perf_counter()
-
     for link in mp3_links:
         file_name = link.split('/')[-1]
         downloaded_file_name = os.getcwd() + "/downloaded/" + file_name
@@ -51,7 +48,9 @@ def download_mp3_series(mp3_links):
 
 
 # Driver method
-#TODO: Make a simple command line interface to get URL
 if __name__ == "__main__":
-    download_mp3_series(get_mp3_links())
-    input('Press a button to exit.')
+    archive_url = input('Enter the page URL which you want to crawl into its HTML:')
+    file_type = input('Enter the file extension for the files which you want to download:')
+    print('downloader will download the files with "' + file_type + '" in "' + archive_url + '".')
+    # download_mp3_series(get_file_links('http://www.dinimizislam.com/detay.asp?Aid=13613', '.mp3'))
+    download_mp3_series(get_file_links(archive_url, file_type))
